e9b1f149422673f12a4a5886ef73f38eb1ecec20
Stephen Guerra <rguerra712>
2016-03-31 19:33:44 -0500

    Example updated
    A basic example of best practices enforced via the AssemblyInfo.cs
    (harsh)

---
+++ EnforceGoodPractices/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace EnforceGoodPractices
+{
+    class Program
+    {
+        static void Main()
+        {
+            //dynamic thisWillNotCompile = "why not?";
+            //Console.WriteLine(thisWillNotCompile);
+
+            //Thread.Sleep(1000); // This will throw
+            //GC.Collect(); // This will throw as well
+        }
+    }
+}
---
+++ EnforceGoodPractices/Properties/AssemblyInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EnforceGoodPractices")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnforceGoodPractices")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e454fef0-adf5-4dec-8575-546184d30dca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+internal class dynamic : object
+{
+
+}
+
+internal class Thread
+{
+    public static void Sleep(int milliseconds)
+    {
+        throw new System.Exception("No sleeping at work!");
+    }
+}
+
+internal class GC
+{
+    public static void Collect()
+    {
+        throw new System.Exception("Are you sure you should be doing this? Are you just guessing at this point?");
+    }
+}
